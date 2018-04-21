0612bffb3c95fdc4f4587566dae4ec552d05052c
danielreznikov <daniel.reznikov@gmail.com>
2018-04-20 17:48:36 -0700

    testing measurement overheard

--- measurement_overhead.c
+++ measurement_overhead.c
@@ -5,18 +5,41 @@
  */
 
 #include <stdio.h>
+#include <inttypes.h>
+#include "measuretime.c"
 
-int readTimeOverhead() {
+void readTimeOverhead() {
+   printf("================================");
+   printf("\nRead Time Overhead Experiments.\n");
+   
+   int expNo = 0;
+   uint64_t strt, end, total, aggregate;
+   int i = 0;
 
+   for (; expNo < 10; ++expNo) {
+      strt = end = total = 0;
+
+      for (; i < 1000000; ++i) {
+         strt = rdtsc();
+         end = rdtsc();
+         total += end - strt;
+      }
+
+      // Output experiment stats.
+      total /= 1000000;
+      aggregate += total;
+      printf("ExpNo(%d) Total (%f)\n", expNo, total);
+   }
+
+   printf("Aggregate Stats: Avg(%f) STD()", aggregate/expNo );
 }
 
-int loopOverhead() {
-
+void loopOverhead() {
+   return NULL;
 }
 
 
 int main() {
-
-
-
+   reatTimeOverhead();
+   return 0;
 }

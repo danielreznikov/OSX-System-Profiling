d3ecd5c1030abd4b11040500a6304ec2ec9dc41a
danielreznikov <daniel.reznikov@gmail.com>
2018-05-10 14:03:44 -0700

    removed exit call that was preventing other modules from running

--- section4_2/RAM_access_time.c
+++ section4_2/RAM_access_time.c
@@ -19,7 +19,6 @@ double experiment_iter(int *arr, int arr_sz, uint64_t iterations, int stride_sz)
    int i, j, idx, randm, ptr = 0;
    double total;
    
-   
    strt = rdtsc();
 
    // An iteration is a single ptr update.
@@ -34,7 +33,6 @@ double experiment_iter(int *arr, int arr_sz, uint64_t iterations, int stride_sz)
    result = end - strt;
    total = (double)result / (double)iterations;
    //total = total - 2 * READ_TIME_OVERHEAD - LOOP_OVERHEAD;
-   //printf("\n\tTOTAL(%lf)", total);
    return total;
 }
 
@@ -77,83 +75,6 @@ void measure_RAM_access(uint64_t experiments, uint64_t iterations) {
          printf("\n\tARRAY_SIZE(%luKB), AVG(%lf), STD(%f) \n", arr_sz/(long)OneKB, avg, std);
 
          free(arr);
-
       }
    }
-   /* DELETE ME */
-   exit(1);
-}
-
-
-/* 4.2.1 - RAM Access Time */
-void _old_measure_RAM_access(uint64_t experiments, uint64_t iterations) {
-   double result, avg, std;
-   int i, expNo, arr_sz_idx, stride_sz, arr_sz;
-
-   // Array sizes, 20 values representing 1KB->512MB arrays.
-   int OneKB = 1024;
-   int L1_SIZE = 32 * OneKB;        // 32KB
-   int L2_SIZE = 256 * OneKB;       // 256KB
-   int L3_SIZE = 6 * OneKB * OneKB; // 6MB
-   int num_sizes = 20;
-   int arr_sizes[num_sizes];
-   for (i = 0; i < num_sizes; i++) {
-      arr_sizes[i] = OneKB * pow(2, i);
-   }
-   
-   // Stride parameter used in array traversal.
-   int L1_STRIDE = 10;                               // 10B
-   int L2_STRIDE = 64*OneKB/sizeof(int) ;            // 64KB
-   int L3_STRIDE = OneKB * OneKB / sizeof(int);      // 1MB
-   int L4_STRIDE = 10 * OneKB * OneKB / sizeof(int); // 10MB
-
-   printHeader("4.2.1 - RAM Access Time");
-
-   // Loop over array size experiments.
-   for (arr_sz_idx = 0; arr_sz_idx < num_sizes; arr_sz_idx++) {
-      // Instantiate an array of appropriate size.
-      arr_sz = arr_sizes[arr_sz_idx];
-      int *arr = (int*)malloc(arr_sz*sizeof(int));
-      prepare_array(arr, arr_sz);
-
-      // Set appropriate stride for region of memory.
-      if (arr_sz < L1_SIZE)
-         stride_sz = L1_STRIDE;
-      else if (arr_sz < L2_SIZE)
-         stride_sz = L2_STRIDE;
-      else if (arr_sz < L3_SIZE)
-         stride_sz = L3_STRIDE;
-      else 
-         stride_sz = L4_STRIDE;
-
-      result = experiment_iter(arr, arr_sz, iterations, stride_sz);
-
-      printf("\nARRAY_SIZE(%dKB), STRIDE(%d), CYCLES(%lf)\n", arr_sz/OneKB, stride_sz, result);
-
-      free(arr);
-
-
-   }
-   /* DELETE ME */
-   exit(1);
-}
-
-
-uint64_t _old_experiment_iter(int *arr, int arr_sz, uint64_t iterations, int stride_sz) {
-   uint64_t total, strt, end;
-   int idx, ptr = 0;
-
-   strt = rdtsc();
-
-   for (int i = 0; i < iterations; i++) {
-      idx = (ptr + stride_sz) % arr_sz;
-      ptr = arr[idx];
-   }
-
-   end = rdtsc();
-
-   total = (end - strt);
-   total /= iterations;
-   total = total - 2* READ_TIME_OVERHEAD - LOOP_OVERHEAD;
-   return total;
 }

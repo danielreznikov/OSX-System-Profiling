39ae6b82040b75ad66c92d4894d35d2d16739951
danielreznikov <daniel.reznikov@gmail.com>
2018-05-13 11:48:55 -0700

    section 4 starter files

---
+++ section4_4/driver_4.c
@@ -0,0 +1,22 @@
+/*
+ * Final Project | CSE 221 | Spring 2018 | UCSD
+ *
+ * Main driver file for 4.2 - Memory Measurements.
+ */
+
+#include "driver_4.h"
+
+/* Main driver and formatting for project output */
+int main() {
+    printf("==========CSE 221 Final Project Measurements - Section 4.2==========\n");
+
+    /* 4.4.1 - Size of File Cache */
+
+    /* 4.4.2 - File Read Time */
+
+    /* 4.4.3 - Remote File Read Time */
+
+    /* 4.4.4 - Contention */
+
+    return 0;
+}
---
+++ section4_4/driver_4.h
@@ -0,0 +1,46 @@
+/*
+ * Final Project | CSE 221 | Spring 2018 | UCSD
+ *
+ * Header file that declares all of the 4.2 measurement code.
+ */
+
+#ifndef DRIVER_2_H
+#define DRIVER_2_H
+
+/* Generic includes */
+#include <emmintrin.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <pthread.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+/* OSX includes only */
+#include <mach/mach_time.h>
+#include <mach/mach.h>
+#include <mach/thread_policy.h>
+
+/* Constants */
+#define EXPERIMENTS     10
+#define ITERATIONS      10
+
+/* 4.2.1 - RAM Access Time */
+void measure_RAM_access(uint64_t experiments, uint64_t iterations);
+
+/* 4.2.2 - RAM Bandwidth */
+void measure_RAM_bandwidth(uint64_t experiments, uint64_t iterations);
+
+/* 4.2.3 - Page Fault Service Time */
+void measure_page_fault(uint64_t experiments);
+
+#endif

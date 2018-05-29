7ecd92ee6733f035f039381e95d23e9a491dd3b9
danielreznikov <daniel.reznikov@gmail.com>
2018-05-29 13:49:09 -0700

    progress towards contention code, runs but still buggy

--- section4_4/contention.c
+++ section4_4/contention.c
@@ -12,80 +12,86 @@
 #include "../utils.h"
 #include "driver_4.h"
 
+/* Child process entry point, returns time to read. */
+unsigned long runChildProcess(int proc_num) {
+	int bytes_to_read = 4 * pow(2,10); // 4KB
+   uint64_t strt, end;
+
+   FILE *fp;
+   char fname[11];
+   sprintf(fname, "data/proc%d", proc_num); 
+   fp = fopen(fname, "rb");
+
+   void *buf = malloc(bytes_to_read);
+
+   strt = rdtsc();
+   fread(buf, bytes_to_read, 1, fp);
+   end = rdtsc();
+
+   //write(fds[i][WRITE], end - strt, sizeof(uint64_t));
+   //exit(1);
+   //close(fds[i][WRITE]);
+   
+   fclose(fp);
+
+   return (unsigned long)(end - strt);
+}
+
 /* Spawn contending procs, return averaged measured read time */
 unsigned long experiment_iter(int num_processes) {
 	pid_t childpids[num_processes];
-	uint64_t strt, end, total_time;
-	int fds[num_processes][2];
-	int bytes_to_read = 4 * pow(2,10); // 4KB
-
-/*
-/* Start children. */
-for (i = 0; i < n; ++i) {
-  if ((pids[i] = fork()) < 0) {
-    perror("fork");
-    abort();
-  } else if (pids[i] == 0) {
-    DoWorkInChild();
-    exit(0);
-  }
-}
-
-/* Wait for children to exit. */
-int status;
-pid_t pid;
-while (n > 0) {
-  pid = wait(&status);
-  printf("Child with PID %ld exited with status 0x%x.\n", (long)pid, status);
-  --n;  // TODO(pts): Remove pid from the pids array.
-}
-*/
-
-
+	unsigned long results[num_processes];
+   unsigned long avg, std;
+	//int fds[num_processes][2];
 
 	for (int i = 0; i < num_processes; i++) {
 		// Generate an 8MB file for this proc to read from.
-		exec("./generatedata " i);
-		
+      char gen_data_script[100] = {0};
+      sprintf(gen_data_script, "sh generate_data.sh %d &>/dev/null", i); 
+		system(gen_data_script);
+
 		// Create a communication channel between parent and child.
-		pipe(fds[i]);		
+		// pipe(fds[i]);		
 
-		// Parent is executing.
-		if ((childpids[i] = fork()) != 0) {
-			close(fds[i][WRITE]);
-         
-			// Read from empty pipe to force a context switch.
-			read(fds[i][READ], &end, sizeof(int));
-		}
-
-		// Child is executing.
-		else {
-			close(fds[i][READ]);
-
-			FILE *fp;
-			char fname[11];
-			sprintf(fname, "data/proc%d", i); 
-			fp = fopen(fname, "rb");
-
-			void *buf = malloc(bytes_to_read);
-
-			strt = rdtsc();
-			fread(buf, bytes_to_read, 1, fp);
-			end = rdtsc();
-
-			write(fds[i][WRITE], end - strt, sizeof(uint64_t));
-			exit(1);
-		}
-	}
-	
+		if ((childpids[i] = fork()) < 0) {
+         perror("fork");
+         abort();
+      }
+      else if (childpids[i] == 0) {
+         results[i] = runChildProcess(i);
+         exit(0);
+      }
+   
+      // Wait for children to exit. 
+      int status;
+      pid_t pid;
+      while (num_processes > 0) {
+         pid = wait(&status);
+         perror("wait");
+         printf("Child with PID %ld exited with status 0x%x.\n", (long)pid, status);
+         --num_processes; 
+      }
+   }
+   // Read from empty pipe to force a context switch.
+	// read(fds[i][READ], &end, sizeof(int));
+   
 	// Aggregate results.
+   stats_long(results, 1, &avg, &std);
 
+   // Cleanup files.
+	for (int i = 0; i < num_processes; i++) {
+      char fname[100] = {0};
+      sprintf(fname, "data/proc%d", i); 
+      remove(fname);
+   }
+
+   return avg;
 }
 
 /* Driver for Contention experiment */
 void measure_read_contention() {
 	int num_processes;
-	int contention_list[] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024};
+	int contention_list[] = {1, 2, 4, 8}; // TODO, 16, 32, 64, 128, 256, 512, 1024};
 	int num_experiments = sizeof(contention_list) / sizeof(int);
 	unsigned long results[num_experiments];
 	unsigned long res;
@@ -94,9 +100,10 @@ void measure_read_contention() {
 
 	for (int i = 0; i < num_experiments; i++) {
 		num_processes = contention_list[i];
+      printf("\n");  
 		res = experiment_iter(num_processes);
 		results[i] = res;		
-		printf("Contentding Procs(%d), Read Time(%lu)", num_processes, res);
+		printf("Contentding Procs(%d), Read Time(%lu)\n", num_processes, res);
 	}
 }
 
